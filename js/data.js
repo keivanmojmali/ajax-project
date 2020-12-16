a52d00cf82d615e8ce7d5ce19a6fa20bd7763d7a
Keivan Mojmali <mojmalik@gmail.com>
2020-12-16 11:23:25 -0800

    data.js is set up

--- js/data.js
+++ js/data.js
@@ -1,1 +1,20 @@
 /* exported data */
+
+
+var user = {
+  profile: {
+    username: '',
+    imgUrl: '',
+    bio: '',
+  },
+  favorites = []
+};
+
+
+returningUser = localStorage.getItem('data');
+user = JSON.parse(returningUser);
+
+
+window.addEventListener('beforeunload', function(e){
+  localStorage.setItem('data',JSON.stringify(user));
+})
